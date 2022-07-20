de0c2c7ed5594c0bd17c2a1027425b72712eb6b0
欧锐 <1447549515@qq.com>
2022-07-20 22:01:36 +0800

    reorder lock sequence, latch_ first -- then page latch; PASS Concurrent Insert

--- src/include/storage/index/b_plus_tree.h
+++ src/include/storage/index/b_plus_tree.h
@@ -133,10 +133,9 @@ class BPlusTree {
                                        Transaction *transaction = nullptr, OpType op_type = OpType::FIND);
   B_PLUS_TREE_LEAF_PAGE_TYPE *FindLeftMostLeaf();
   B_PLUS_TREE_LEAF_PAGE_TYPE *FindRightMostLeaf();
-  BPlusTreePage *FindRoot(const KeyType &key, Transaction *transaction, OpType op_type);
   template <typename N>
   bool Safe(N *node, OpType op_type = OpType::FIND) const;
-  void ReleaseAllPages(Transaction *transaction) const;
+  void ReleaseAllPages(Transaction *transaction);
 
   // member variable
   std::string index_name_;
